3bd4a85f7d8e51c763b3e682078d6b1493f21dfa
code-chronos <128954619+code-chronos@users.noreply.github.com>
2025-02-06 21:51:36 +0530

    wip Feb 6th 2025

--- test1.js
+++ test1.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const csv = require('csvtojson');
+const { json } = require('stream/consumers');
 
 csv()
   .fromFile("shameless_cloning_datadump" + '.csv')
@@ -13,9 +14,22 @@ csv()
       };
     });
 
-    const forSingleInvestor = addedMissingFieldToJson.filter(jsObject => jsObject.InvestorName === "Vijay Kishanlal Kedia")
+    // console.log(addedMissingFieldToJson);
+
+    // console.log(addedMissingFieldToJson.InvestorName);
+
+
+    const investorNamesSet = new Set();
+
+    //  Follow the code on 25th line on 07-02-2025
+    //  Extract all the names of investors from addedMissingFieldToJson and add it to investorNamesSet and then modify the code in line 27
+
+    // const forSingleInvestor = addedMissingFieldToJson.filter(jsObject => jsObject.InvestorName === "Vijay Kishanlal Kedia")
 
     let sorted = forSingleInvestor.sort((a, b) => {
+
+      //let sorted = forSingleInvestor.sort((a, b) => {
+
       const aParsed = parseFloat(a["Dec 2023"].replace("%", "").replace("-", "0"));
       const bParsed = parseFloat(b["Dec 2023"].replace("%", "").replace("-", "0"));
       return bParsed - aParsed;
@@ -37,19 +51,14 @@ csv()
         }
       };
     });
-
     // console.log(JSON.stringify(modifiedObjCompany, null, 2));
-
     // const modifiedObjCompanyRanks1 = modifiedObjCompany.map(obj => {
     //   const objKey = Object.keys(obj)[0];
     //   obj[objKey].ranks = {};
     //   return obj;
     // })
-
     const FirstObjRanks1 = modifiedObjCompany[0];
-
     const months = Object.keys(Object.values(FirstObjRanks1)[0].percents);
-
     // console.log(months);
 
     let rankingForIndividualMonth
@@ -67,5 +76,8 @@ csv()
         return obj;
       })
     }
-    console.log("rank", JSON.stringify(rankingForIndividualMonth, null, 2));
+    // console.log("rank", JSON.stringify(rankingForIndividualMonth, null, 2));
+
+
+
   })
